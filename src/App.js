aace5c8ca3d8fa3a85a7ea58d8fe682205768bed
xjeyceex <jcmiguel.beltran@gmail.com>
2025-06-07 16:49:42 +0800

    tests

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { CartProvider } from './context/CartContext';
 
 export default function App() {
   return (
-   <CartProvider>
-    <main className="p-4 max-w-7xl mx-auto flex gap-8">
-      <div className="flex-[2]">
-        <h1 className="text-2xl font-bold mb-4">Product Store</h1>
-        <ProductList />
-      </div>
-      <aside className="flex-[1] sticky top-4 self-start border p-4 rounded shadow max-w-md">
-        <Cart />
-      </aside>
-    </main>
-  </CartProvider>
+    <CartProvider>
+      <main className="py-4 px-12 flex gap-8 w-full">
+        <div className="flex-[2]">
+          <h1 className="text-2xl font-bold mb-4">Product Store</h1>
+          <ProductList />
+        </div>
+        <aside className="flex-[1] sticky top-16 self-start border p-4 rounded shadow">
+          <Cart />
+        </aside>
+      </main>
+    </CartProvider>
   );
 }
